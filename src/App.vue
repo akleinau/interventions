dbc52b21bebfa774e1a33a994982ff5641b7de3c
akleinau <56925445+akleinau@users.noreply.github.com>
2025-05-07 15:30:03 +0200

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
+ const address = "http://127.0.0.1:5000/"
+
+ async function get_input_params() {
+   let gResponse = null
+   gResponse = await fetch(address + "get_input_params");
+   const response = await gResponse.json();
+    console.log(response)
+
+ }
+
+
 </script>
 
 <template>
   <div>
     Hello World!
   </div>
+
+  <v-btn @click="get_input_params">Get Input Params</v-btn>
+
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,33 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
+export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  },
+})
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(vuetify)
+
+app.mount('#app')
