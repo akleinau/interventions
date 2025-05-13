7fffbe284886aef45a388745727bce4a53315fa3
akleinau <56925445+akleinau@users.noreply.github.com>
2025-05-13 17:07:31 +0200

    refactor

---
+++ src/components/data_input_items.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+
+import {useDataStore} from "../stores/data_store";
+import {onMounted, ref} from "vue";
+
+let dataStore = useDataStore()
+
+let lbl = dataStore.label
+let expln = dataStore.explanation
+
+const props = defineProps(['param'])
+
+</script>
+
+<template>
+  <v-col cols="3" class="pa-0">
+    <div class="text-align-last align-end w-100 mt-2 pa-0 pr-3" style="text-align:right"> {{ lbl(props.param.label) }}</div>
+  </v-col>
+
+  <v-col cols="9" class="pa-0">
+
+    <!-- slider -->
+    <div v-if="param.type == 'slider'" class="w-100 pa-0">
+
+      <v-slider v-model="dataStore.input_params[param.id]" :min="param.min" :max="param.max" :step="param.step"
+                thumb-label thumb-size="20" class="pa-0"
+                density="compact" hide-details single-line color="grey-darken-1">
+        <template v-slot:append>
+          <v-text-field v-model="dataStore.input_params[param.id]" density="compact" variant="plain"
+                        style="width: 50px" type="number" hide-details single-line class="mb-2"
+          ></v-text-field>
+        </template>
+      </v-slider>
+    </div>
+
+    <!-- checkbox -->
+    <div v-else-if="param.type == 'checkbox'">
+      <v-checkbox v-model="dataStore.input_params[param.id]"
+                  density="compact" hide-details single-line/>
+    </div>
+
+    <!-- select -->
+    <div v-else-if="param.type == 'select'" class="d-flex mr-5 mt-2">
+      <v-select v-model="dataStore.input_params[param.id]" :items="param.choices" :label="param.label"
+                density="compact" hide-details single-line class="mb-2" variant="outlined">
+        <template v-slot:item="{ props: itemProps, item }">
+          <v-hover v-slot="{ isHovering, props }">
+            <div v-bind="props">
+              <v-list-item v-bind="itemProps" title="">
+                <div style="width:600px">
+                  <span class="mr-2">{{ lbl(item.value) }}  </span>
+                  <span class="text-grey" style="font-size:12px; width:50px"
+                        v-show="isHovering">{{ expln(item.value) }}</span>
+                </div>
+              </v-list-item>
+            </div>
+          </v-hover>
+        </template>
+      </v-select>
+    </div>
+
+    <!-- multiselect -->
+    <div v-else-if="param.type == 'multiselect'" class="d-flex mr-5 w-100">
+      <v-select v-model="dataStore.input_params[param.id]" :items="param.choices" :label="param.label"
+                active-class="bg-yellow"
+                density="compact" hide-details single-line multiple class="mb-2" variant="underlined">
+        <template v-slot:selection="{ item, index }">
+          <v-chip v-if="index < 5" :text="lbl(item.value)" class="mb-1"></v-chip>
+          <span v-if="index === 5" class="text-grey text-caption align-self-center">
+                  (+{{ dataStore.input_params[props.param.id].length - 5 }} others)
+                </span>
+        </template>
+        <template v-slot:item="{ props: itemProps, item }">
+          <v-hover v-slot="{ isHovering, props }">
+            <div v-bind="props">
+              <v-list-item v-bind="itemProps" title="">
+                <div style="width:600px">
+                  <span class="mr-2">{{ lbl(item.value) }}  </span>
+                  <span class="text-grey" style="font-size:12px; width:50px"
+                        v-show="isHovering">{{ expln(item.value) }}</span>
+                </div>
+              </v-list-item>
+            </div>
+          </v-hover>
+        </template>
+      </v-select>
+    </div>
+
+  </v-col>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/data_loading.vue
+++ src/components/data_loading.vue
@@ -2,6 +2,7 @@
 
 import {useDataStore} from "../stores/data_store";
 import {onMounted, ref} from "vue";
+import Data_input_items from "./data_input_items.vue";
 
 let dataStore = useDataStore()
 
@@ -42,9 +43,6 @@ async function get_labels() {
   dataStore.labels = response
 }
 
-let lbl = dataStore.label
-let expln = dataStore.explanation
-
 </script>
 
 <template>
@@ -54,81 +52,7 @@ let expln = dataStore.explanation
       <h2>Parameters</h2>
       <v-container>
         <v-row v-for="(param, index) in input_spec.params" :key="index">
-
-          <v-col cols="3">
-            <div class="text-align-last align-end w-100 mt-2" style="text-align:right"> {{ lbl(param.label) }}</div>
-          </v-col>
-
-          <v-col cols="9">
-
-             <!-- slider -->
-            <div v-if="param.type == 'slider'" class="w-100">
-
-              <v-slider v-model="dataStore.input_params[param.id]" :min="param.min" :max="param.max" :step="param.step"
-                        thumb-label thumb-size="20"
-                        density="compact" hide-details single-line color="grey-darken-1">
-                <template v-slot:append>
-                  <v-text-field v-model="dataStore.input_params[param.id]" density="compact" variant="plain"
-                                style="width: 50px" type="number" hide-details single-line class="mb-2"
-                  ></v-text-field>
-                </template>
-              </v-slider>
-            </div>
-
-            <!-- checkbox -->
-            <div v-else-if="param.type == 'checkbox'">
-              <v-checkbox v-model="dataStore.input_params[param.id]"
-                          density="compact" hide-details single-line/>
-            </div>
-
-               <!-- select -->
-            <div v-else-if="param.type == 'select'" class="d-flex mr-5 mt-2">
-              <v-select v-model="dataStore.input_params[param.id]" :items="param.choices" :label="param.label"
-                        density="compact" hide-details single-line class="mb-2" variant="outlined">
-                <template v-slot:item="{ props: itemProps, item }" >
-                  <v-hover v-slot="{ isHovering, props }">
-                    <div v-bind="props">
-                    <v-list-item v-bind="itemProps" title="">
-                      <div style="width:600px">
-                        <span class="mr-2">{{ lbl(item.value) }}  </span>
-                        <span class="text-grey" style="font-size:12px; width:50px"
-                              v-show="isHovering">{{ expln(item.value) }}</span>
-                      </div>
-                    </v-list-item>
-                      </div>
-                  </v-hover>
-                </template>
-              </v-select>
-            </div>
-
-            <!-- multiselect -->
-            <div v-else-if="param.type == 'multiselect'" class="d-flex mr-5 w-100">
-              <v-select v-model="dataStore.input_params[param.id]" :items="param.choices" :label="param.label"
-                        active-class="bg-yellow"
-                        density="compact" hide-details single-line multiple class="mb-2" variant="underlined">
-                <template v-slot:selection="{ item, index }">
-                  <v-chip v-if="index < 5" :text="lbl(item.value)" class="mb-1"></v-chip>
-                  <span v-if="index === 5" class="text-grey text-caption align-self-center">
-                  (+{{ dataStore.input_params[param.id].length - 5 }} others)
-                </span>
-                </template>
-                <template v-slot:item="{ props: itemProps, item }" >
-                  <v-hover v-slot="{ isHovering, props }">
-                    <div v-bind="props">
-                    <v-list-item v-bind="itemProps" title="">
-                      <div style="width:600px">
-                        <span class="mr-2">{{ lbl(item.value) }}  </span>
-                        <span class="text-grey" style="font-size:12px; width:50px"
-                              v-show="isHovering">{{ expln(item.value) }}</span>
-                      </div>
-                    </v-list-item>
-                      </div>
-                  </v-hover>
-                </template>
-              </v-select>
-            </div>
-
-          </v-col>
+            <data_input_items :param="param" class="pa-0" />
         </v-row>
       </v-container>
 
@@ -140,82 +64,7 @@ let expln = dataStore.explanation
       <h2>Interventions</h2>
       <v-container>
         <v-row v-for="(param, index) in input_spec.interventions" :key="index">
-
-          <v-col cols="3">
-            <div class="text-align-last align-end w-100 mt-2" style="text-align:right"> {{ lbl(param.label) }}</div>
-          </v-col>
-
-          <v-col cols="9">
-
-            <!-- slider -->
-            <div v-if="param.type == 'slider'" class="w-100">
-
-              <v-slider v-model="dataStore.input_params[param.id]" :min="param.min" :max="param.max" :step="param.step"
-                        thumb-label thumb-size="20"
-                        density="compact" hide-details single-line color="grey-darken-1">
-                <template v-slot:append>
-                  <v-text-field v-model="dataStore.input_params[param.id]" density="compact" variant="plain"
-                                style="width: 50px" type="number" hide-details single-line class="mb-2"
-                  ></v-text-field>
-                </template>
-              </v-slider>
-            </div>
-
-            <!-- checkbox -->
-            <div v-else-if="param.type == 'checkbox'">
-              <v-checkbox v-model="dataStore.input_params[param.id]"
-                          density="compact" hide-details single-line/>
-            </div>
-
-                    <!-- select -->
-            <div v-else-if="param.type == 'select'" class="d-flex mr-5 mt-2">
-              <v-select v-model="dataStore.input_params[param.id]" :items="param.choices" :label="param.label"
-                        density="compact" hide-details single-line class="mb-2" variant="outlined">
-                <template v-slot:item="{ props: itemProps, item }" >
-                  <v-hover v-slot="{ isHovering, props }">
-                    <div v-bind="props">
-                    <v-list-item v-bind="itemProps" title="">
-                      <div style="width:600px">
-                        <span class="mr-2">{{ lbl(item.value) }}  </span>
-                        <span class="text-grey" style="font-size:12px; width:50px"
-                              v-show="isHovering">{{ expln(item.value) }}</span>
-                      </div>
-                    </v-list-item>
-                      </div>
-                  </v-hover>
-                </template>
-              </v-select>
-            </div>
-
-            <!-- multiselect -->
-            <div v-else-if="param.type == 'multiselect'" class="d-flex mr-5 w-100">
-              <v-select v-model="dataStore.input_params[param.id]" :items="param.choices" :label="param.label"
-                        active-class="bg-yellow"
-                        density="compact" hide-details single-line multiple class="mb-2" variant="underlined">
-                <template v-slot:selection="{ item, index }">
-                  <v-chip v-if="index < 5" :text="lbl(item.value)" class="mb-1"></v-chip>
-                  <span v-if="index === 5" class="text-grey text-caption align-self-center">
-                  (+{{ dataStore.input_params[param.id].length - 5 }} others)
-                </span>
-                </template>
-                <template v-slot:item="{ props: itemProps, item }" >
-                  <v-hover v-slot="{ isHovering, props }">
-                    <div v-bind="props">
-                    <v-list-item v-bind="itemProps" title="">
-                      <div style="width:600px">
-                        <span class="mr-2">{{ lbl(item.value) }}  </span>
-                        <span class="text-grey" style="font-size:12px; width:50px"
-                              v-show="isHovering">{{ expln(item.value) }}</span>
-                      </div>
-                    </v-list-item>
-                      </div>
-                  </v-hover>
-                </template>
-              </v-select>
-            </div>
-
-          </v-col>
-
+          <data_input_items :param="param" class="pa-0" />
         </v-row>
       </v-container>
     </div>
