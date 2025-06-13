d13fdbeffcb0551b14ac74d9f2a8af4b5ca0393b
akleinau <56925445+akleinau@users.noreply.github.com>
2025-06-13 18:23:05 +0200

    fix github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/Interventions/'
+  base: '/interventions/'
 })
